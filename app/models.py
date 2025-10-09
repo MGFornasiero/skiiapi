adbeb52d400f22b50615e5ad2febb29f5939793e
MGFornasiero <mg.fornasiero@gmail.com>
2025-10-09 14:35:21 +0200

    Enhance model methods with from_sql_row for data retrieval and improve project path handling in tests

--- app/models.py
+++ app/models.py
@@ -107,7 +107,7 @@ class DetailedNotes(BaseModel):
 # =============================================================
 
 
-class Target(BaseModel): # ski.targets() , public.get_targets(), public.get_target_info(), public.qry_ts_targets()
+class Target(BaseModel): # ski.targets , public.get_targets(), public.get_target_info(), public.qry_ts_targets()
     id_target: int
     name: str
     original_name: Optional[str] = None
@@ -140,6 +140,15 @@ class StrikingPart(BaseModel): # ski.strikingparts() , public.get_strikingparts(
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in StrikingPart.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'StrikingPart':
+        return StrikingPart(
+            id_part=row[0],
+            name=row[1],
+            translation=row[2],
+            description=row[3],
+            notes=row[4],
+            resource_url=row[5]
+        )
 
 
 class Technic(BaseModel): # ski.technics() , public.get_technics(), public.get_technic_info(), public.qry_ts_technics()
@@ -153,6 +162,15 @@ class Technic(BaseModel): # ski.technics() , public.get_technics(), public.get_t
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in Technic.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'Technic':
+        return Technic(
+            id_technic=row[0],
+            waza=row[1],
+            name=row[2],
+            description=row[3],
+            notes=row[4],
+            resource_url=row[5]
+        )  
 
 class TechnicDecomposition(BaseModel):
     id_decomposition: int
@@ -166,6 +184,16 @@ class TechnicDecomposition(BaseModel):
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in TechnicDecomposition.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'TechnicDecomposition':
+        return TechnicDecomposition(
+            id_decomposition=row[0],
+            technic_id=row[1],
+            component_order=row[2],
+            description=row[3],
+            explatations=row[4],
+            notes=row[5],
+            resource_url=row[6]
+        )
 
 
 class Stand(BaseModel):# ski.stands() , public.get_stands(), public.get_stand_info(), public.qry_ts_stands()
@@ -178,6 +206,14 @@ class Stand(BaseModel):# ski.stands() , public.get_stands(), public.get_stand_in
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in Stand.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'Stand':
+        return Stand(
+            id_stand=row[0],
+            name=row[1],
+            description=row[2],
+            illustration_url=row[3],
+            notes=row[4]
+        )  
 
 class Grade(BaseModel): # ski.grades() , public.get_grade()
     id_grade: int
@@ -188,6 +224,13 @@ class Grade(BaseModel): # ski.grades() , public.get_grade()
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in Grade.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'Grade':
+        return Grade(
+            id_grade=row[0],
+            gtype=row[1],
+            grade=row[2],
+            color=row[3]
+        )
 
 
 class KihonInventory(BaseModel): # ski.kihon_inventory() , public.get_kihons()
@@ -200,6 +243,14 @@ class KihonInventory(BaseModel): # ski.kihon_inventory() , public.get_kihons()
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KihonInventory.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KihonInventory':
+        return KihonInventory(
+            id_inventory=row[0],
+            grade_id=row[1],
+            number=row[2],
+            resources=row[3],
+            notes=row[4]
+        )
 
 class KihonSequence(BaseModel):
     id_sequence: int
@@ -217,6 +268,20 @@ class KihonSequence(BaseModel):
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KihonSequence.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KihonSequence':
+        return KihonSequence(
+            id_sequence=row[0],
+            inventory_id=row[1],
+            seq_num=row[2],
+            stand_id=row[3],
+            technic_id=row[4],
+            hips=row[5],
+            gyaku=row[6],
+            target_hgt=row[7],
+            resources=row[8],
+            notes=row[9],
+            resource_url=row[10]
+        )
 
 class KihonTx(BaseModel):#get_kihon_tx()
     id_tx: int
@@ -231,6 +296,17 @@ class KihonTx(BaseModel):#get_kihon_tx()
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KihonTx.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KihonTx':
+        return KihonTx(
+            id_tx=row[0],
+            from_sequence=row[1],
+            to_sequence=row[2],
+            movement=row[3],
+            resources=row[4],
+            notes=row[5],
+            tempo=row[6],
+            resource_url=row[7]
+        )   
 
 class KihonStep(BaseModel): #get_kihon_steps()
     id_sequence: int
@@ -248,6 +324,20 @@ class KihonStep(BaseModel): #get_kihon_steps()
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KihonStep.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KihonStep':
+        return KihonStep(
+            id_sequence=row[0],
+            inventory_id=row[1],
+            seq_num=row[2],
+            stand_id=row[3],
+            technic_id=row[4],
+            gyaku=row[5],
+            target_hgt=row[6],
+            notes=row[7],
+            resource_url=row[8],
+            stand_name=row[9],
+            technic_name=row[10]
+        )
 
 class KihonFormatted(BaseModel): #kihon_frmlist()
     number: int
@@ -264,6 +354,19 @@ class KihonFormatted(BaseModel): #kihon_frmlist()
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KihonFormatted.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KihonFormatted':
+        return KihonFormatted(
+            number=row[0],
+            seq_num=row[1],
+            movement=row[2],
+            technic_id=row[3],
+            gyaku=row[4],
+            tecnica=row[5],
+            stand_id=row[6],
+            posizione=row[7],
+            target_hgt=row[8],
+            notes=row[9]
+        )
 
 
 class KataInventory(BaseModel): # ski.kata_inventory() , public.show_katainventory(), public.get_katainfo()
@@ -278,6 +381,16 @@ class KataInventory(BaseModel): # ski.kata_inventory() , public.show_katainvento
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KataInventory.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KataInventory':
+        return KataInventory(
+            id_kata=row[0],
+            kata=row[1],
+            serie=row[2],
+            starting_leg=row[3],
+            notes=row[4],
+            resources=row[5],
+            resource_url=row[6]
+        )
 
 class KataSequence(BaseModel):
     id_sequence: int
@@ -298,6 +411,23 @@ class KataSequence(BaseModel):
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KataSequence.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KataSequence':
+        return KataSequence(
+            id_sequence=row[0],
+            kata_id=row[1],
+            seq_num=row[2],
+            stand_id=row[3],
+            speed=row[4],
+            side=row[5],
+            hips=row[6],
+            embusen=row[7],
+            facing=row[8],
+            kiai=row[9],
+            notes=row[10],
+            remarks=row[11],
+            resources=row[12],
+            resource_url=row[13]
+        )
 
 class KataSequenceWaza(BaseModel):
     id_kswaza: int
@@ -312,6 +442,17 @@ class KataSequenceWaza(BaseModel):
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KataSequenceWaza.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KataSequenceWaza':
+        return KataSequenceWaza(
+            id_kswaza=row[0],
+            sequence_id=row[1],
+            arto=row[2],
+            technic_id=row[3],
+            strikingpart_id=row[4],
+            technic_target_id=row[5],
+            notes=row[6],
+            resources=row[7]
+        )
 
 class KataTx(BaseModel):
     id_tx: int
@@ -328,6 +469,19 @@ class KataTx(BaseModel):
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KataTx.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KataTx':
+        return KataTx(
+            id_tx=row[0],
+            from_sequence=row[1],
+            to_sequence=row[2],
+            tempo=row[3],
+            direction=row[4],
+            intermediate_stand_id=row[5],
+            notes=row[6],
+            remarks=row[7],
+            resources=row[8],
+            resource_url=row[9]
+        )
 
 class KataTechnique(BaseModel): #json in tecniche di KataSequenceStep
     sequence_id: int
@@ -342,6 +496,17 @@ class KataTechnique(BaseModel): #json in tecniche di KataSequenceStep
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KataTechnique.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KataTechnique':
+        return KataTechnique(
+            sequence_id=row[0],
+            arto=row[1],
+            technic_id=row[2],
+            tecnica=row[3],
+            technic_target_id=row[4],
+            obiettivo=row[5],
+            waza_note=row[6],
+            waza_resources=row[7]
+        )
 
 class KataSequenceStep(BaseModel): #get_katasequence()
     id_sequence: int
@@ -362,6 +527,23 @@ class KataSequenceStep(BaseModel): #get_katasequence()
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in KataSequenceStep.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'KataSequenceStep':
+        return KataSequenceStep(
+            id_sequence=row[0],
+            kata_id=row[1],
+            seq_num=row[2],
+            stand_id=row[3],
+            posizione=row[4],
+            guardia=row[5],
+            facing=row[6],
+            tecniche=row[7],
+            embusen=row[8],
+            kiai=row[9],
+            notes=row[10],
+            remarks=row[11],
+            resources=row[12],
+            resource_url=row[13]
+        )
 
 
 class BunkaiInventory(BaseModel): # ski.bunkai_inventory() , public.get_katabunkais()
@@ -377,6 +559,17 @@ class BunkaiInventory(BaseModel): # ski.bunkai_inventory() , public.get_katabunk
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in BunkaiInventory.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'BunkaiInventory':
+        return BunkaiInventory(
+            id_bunkai=row[0],
+            kata_id=row[1],
+            version=row[2],
+            name=row[3],
+            description=row[4],
+            notes=row[5],
+            resources=row[6],
+            resource_url=row[7]
+        )
 
 class BunkaiSequence(BaseModel):
     id_bunkaisequence: int
@@ -391,3 +584,14 @@ class BunkaiSequence(BaseModel):
     def to_sql_values(self) -> str:
         values = [format_value(getattr(self, field)) for field in BunkaiSequence.model_fields]
         return f"({', '.join(values)})"
+    def from_sql_row(row: tuple) -> 'BunkaiSequence':
+        return BunkaiSequence(
+            id_bunkaisequence=row[0],
+            bunkai_id=row[1],
+            kata_sequence_id=row[2],
+            description=row[3],
+            notes=row[4],
+            remarks=row[5],
+            resources=row[6],
+            resource_url=row[7]
+        )
--- app/test.py
+++ app/test.py
@@ -1,23 +1,27 @@
 from typing import Union
 import os
 import json
+import sys
 import psycopg2
 
+# Add the project root to the Python path
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 
-from .models import Target # ski.targets() , public.get_targets(), public.get_target_info(), public.qry_ts_targets()
-from .models import StrikingPart # ski.strikingparts() , public.get_strikingparts(), public.get_strikingparts_info(), public.qry_ts_strikingparts()
-from .models import Stand # ski.stands() , public.get_stands(), public.get_stand_info(), public.qry_ts_stands()
-from .models import Grade # ski.grades() , public.get_grade()
-from .models import KihonInventory # ski.kihon_inventory() , public.get_kihons()
-from .models import KihonTx #get_kihon_tx()
-from .models import KihonStep  #get_kihon_steps()
-from .models import KihonFormatted  #kihon_frmlist()
-from .models import KataInventory  # ski.kata_inventory() , public.show_katainventory(), public.get_katainfo()
-from .models import KataSequenceStep  #get_katasequence()
-from .models import BunkaiInventory  # ski.bunkai_inventory() , public.get_katabunkais()
+from app.models import Target # ski.targets() , public.get_targets(), public.get_target_info(), public.qry_ts_targets()
+from app.models import StrikingPart # ski.strikingparts() , public.get_strikingparts(), public.get_strikingparts_info(), public.qry_ts_strikingparts()
+from app.models import Stand # ski.stands() , public.get_stands(), public.get_stand_info(), public.qry_ts_stands()
+from app.models import Grade # ski.grades() , public.get_grade()
+from app.models import KihonInventory # ski.kihon_inventory() , public.get_kihons()
+from app.models import KihonTx #get_kihon_tx()
+from app.models import KihonStep  #get_kihon_steps()
+from app.models import KihonFormatted  #kihon_frmlist()
+from app.models import KataInventory  # ski.kata_inventory() , public.show_katainventory(), public.get_katainfo()
+from app.models import KataSequenceStep  #get_katasequence()
+from app.models import BunkaiInventory  # ski.bunkai_inventory() , public.get_katabunkais()
 
 uri = os.environ['SKIURI']
-
 
 
 
@@ -25,6 +29,19 @@ uri = os.environ['SKIURI']
 kata_id = 2
 print("\n")
 print("\n")
+
+
+conn = psycopg2.connect(uri)
+cur = conn.cursor()
+cur.execute("SELECT id_target, name, original_name, description, notes, resource_url FROM public.get_targets();")
+result = cur.fetchall()
+tgts = [Target.from_sql_row(row) for row in result]
+print(tgts)
+
+print("\n")
+
+
+
 conn = psycopg2.connect(uri)
 cur = conn.cursor()
 cur.execute(f"SELECT id_bunkai, kata_id, version, name, description, notes,resources, resource_url FROM public.get_katabunkais({kata_id});")
@@ -43,74 +60,5 @@ bunkai_ids  = {
 print(bunkai_ids)
 bunkai_id = 2
 
-import psycopg2.pool
-
-# Create a connection pool with a minimum of 2 connections and
-#a maximum of 3 connections
-pool = psycopg2.pool.SimpleConnectionPool(
-    1, 5, uri
-)
-
-# Get a connection from the pool
-with my_pool.connection() as conn:
-    conn.execute(...)
-
-# Use the connection to execute a query
-cursor = connection1.cursor()
-print("Results from Connection1: \n")
-cursor.execute('SELECT * FROM person ORDER BY id')
-results = cursor.fetchall()
-for data in results:
-    print(data)
-    print()
-
-connection2 = pool.getconn()
-
-# Use the connection to execute a query
-cursor = connection2.cursor()
-print("Results from Connection2: \n")
-cursor.execute('SELECT * FROM person ORDER BY id')
-results = cursor.fetchall()
-for data in results:
-    print(data)
-    print()
-
-connection3 = pool.getconn()
-
-# Use the connection to execute a query
-cursor = connection3.cursor()
-print("Results from Connection3: \n")
-cursor.execute('SELECT * FROM person ORDER BY id')
-results = cursor.fetchall()
-for data in results:
-    print(data)
-    print()
-
-# Since maximum number of connections in the pool #
-#is 3 and three connections 
-#are already made and not released yet. 
-#So, requesting for a fourth connection gives error.
-
-# connection4 = pool.getconn()
-# cursor = connection4.cursor()
-# print("Results from Connection3: \n")
-# cursor.execute('SELECT * FROM person ORDER BY id')
-# results = cursor.fetchall()
-# for data in results:
-#     print(data)
-#     print()
-
-
-# Release the connection back to the pool
-pool.putconn(connection1)
-pool.putconn(connection2)
-pool.putconn(connection3)
-
-
-
-
-
-
-
-
-
+cur.close()
+conn.close()
